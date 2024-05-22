30b8f8a679c512aa5711ddd039ad885a15bb0909
Guilherme Cutieri <cutieriguilherme@gmail.com>
2024-05-21 23:02:27 -0300

    changes

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+  // script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const flower = document.getElementById('flower');
+  const message = document.getElementById('message');
+
+  // Supondo que você tenha alguma animação de flor aqui
+  // Exibir a mensagem ao mesmo tempo
+  flower.addEventListener('animationend', function() {
+      message.style.display = 'block';
+  });
+});
